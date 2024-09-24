0ccccdb7bf249f07c822f41292c4ebb922818272
Atanurag <anuragt9510@gmail.com>
2024-09-24 23:20:07 +0530

    k

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -106,7 +106,7 @@ const Home = () => {
   return (
     <><div  className= 'category-card' style={{ marginRight:'9px',width:'100px',display:'flex',flexDirection:'column',gap:'6px'}}>
 
-    <div style={{borderRadius:'4px',height:'80px',width:'80px',border:'1px solid black'}}></div>
+    <div style={{  backgroundColor: "#edeef0",borderRadius:'4px',height:'80px',width:'80px',border:'1px solid black'}}></div>
     
     <p style={{fontSize:'15px',fontWeight:'600'}}>Top seller</p>
     </div>
@@ -122,16 +122,17 @@ const Home = () => {
 
 
 {/* overflowX:'auto' */}
-<div style={{ height: 130, backgroundColor: "#edeef0",width:'100%',padding:'15px' }}>
+<div style={{ height: 150, backgroundColor: "#edeef0",width:'100%',padding:'20px 15px', marginBottom:'40px'}}>
   <div
     style={{
       display: "flex",
       justifyContent: "space-between",
-      margin:'10px 0'
+      marginTop:'2px',
+      marginBottom:'15px'
       
     }}
   >
-    <p style={{ fontSize: 16, fontWeight: "bold" }}>Shop by Category</p>
+    <p style={{ fontSize: 16, fontWeight: "bold" }}>Crazy Deals</p>
     <p
       style={{
         fontSize: 14,
@@ -145,16 +146,16 @@ const Home = () => {
   </div>
 
   
-<div className='category-container' style={{display:'flex',justifyContent:'space-around', overflowX:'scroll',}}>
+<div className='category-container' style={{display:'flex',justifyContent:'space-around', overflowX:'scroll'}}>
 {
   Array(4).fill('*').map((e,i)=>{
     return (
-      <><div className='offer-card'  style={{backgroundColor:'#b6babf',display:'flex',minWidth:'200px',alignItems:'center',justifyContent:'space-between',borderRadius:'5px',marginRight:'12px'}}>
+      <><div className='offer-card'  style={{backgroundColor:'rgb(209 209 209)',display:'flex',minWidth:'200px',alignItems:'center',justifyContent:'space-between',borderRadius:'5px',marginRight:'12px'}}>
 
-      <div style={{height:'70px',width:'70px',backgroundColor:'white',borderRadius:'4px'}}></div>
-      <div style={{display:'flex',flexDirection:'column',textAlign:'left',padding:'3px',gap:'3px'}}>
-      <p style={{color:"white",fontSize:'12px'}}>Match Day Mania</p>
-      <p style={{color:'white',fontSize:'15px',fontWeight:'bold'}}>Flat ₹ 125 Off</p>
+      <div style={{ backgroundColor: "#edeef0",height:'70px',width:'70px',backgroundColor:'white',borderRadius:'4px'}}></div>
+      <div style={{display:'flex',flexDirection:'column',textAlign:'left',padding:'5px',gap:'3px'}}>
+      <p style={{fontSize:'12px'}}>Match Day Mania</p>
+      <p style={{fontSize:'15px',fontWeight:'bold'}}>Flat ₹ 125 Off</p>
       
       <span style={{fontSize:'13px'}}>On orders above ₹ 455</span>
       </div>
@@ -176,9 +177,15 @@ const Home = () => {
 
 </div>
 
-gashsa
 
 
+
+<div style={{ left: '50%',transform: 'translateX(-50%)',position:'fixed',bottom :'10px',zIndex:'99999', backgroundColor:'#1677ff',height:'50px',display:'flex',justifyContent:'space-around',alignItems:'center',width:'90%',borderRadius:'4px'}}>
+<span style={{position:'relative'}}> <ShoppingOutlined  style={{fontSize:'22px',color:'white'}}/><div style={{ position: 'absolute', background: 'white', top: 0, borderRadius: '50%', color: 'red', padding: '8px', height: '12px', width: '12px', fontWeight: '800', left: '16px', display: 'flex', justifyContent: "center", alignItems: 'center' }}>{5}</div></span>
+
+<span style={{color:'white',fontWeight:'bold'}}>View your cart</span>
+<span style={{color:'white',fontWeight:'bold'}}>₹ 50</span>
+  </div>
 
 
 
