1c5df12ee1f9a00a64d3f2cd18f272991fd1fcb1
Atanurag <anuragt9510@gmail.com>
2024-09-25 17:58:47 +0530

    ddd

--- src/demo.jsx
+++ src/demo.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import './index.css';
 import { Col, Row } from 'antd';
 import { Routes, Route, Link, useNavigate, json } from 'react-router-dom';
-import Home from './Pages/Home.jsx'
-import Search from './Pages/Search.jsx'
-import Category from './Pages/Category.jsx'
+import Home from './Pages/Home.jsx';
+import Search from './Pages/Search.jsx';
+import Category from './Pages/Category.jsx';
+import Cart from './Pages/Cart.jsx';
 const App = () => (
   <>
   <Routes >
@@ -12,6 +13,7 @@ const App = () => (
 
    <Route path='/search' element={<Search/>}/>
    <Route path='/category' element={<Category/>}/>
+   <Route path='/cart' element={<Cart/>}/>
     </Routes>
   </>
 );
