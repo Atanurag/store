f3e8e6b631f10caee401ac778ac1fdaa992f6c13
Atanurag <anuragt9510@gmail.com>
2024-09-25 18:11:34 +0530

    sss

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import {useState} from 'react';
+import { Divider, Flex, Tag, Button, Layout, Input, Row, Col, Switch, Card, Badge } from 'antd';
+import { ArrowLeftOutlined ,InfoCircleOutlined ,CloseOutlined, MenuUnfoldOutlined, SearchOutlined, ShoppingOutlined } from '@ant-design/icons';
+import { Routes, Route, Link, useNavigate, json } from 'react-router-dom';
 const Cart =()=>{
+    const navigate = useNavigate();
     return(<>
 
-dfdf
+<div style={{color:'white',height:'100px',backgroundColor:'#444444',padding:'0 12px',display:'flex',alignItems:'center' ,justifyContent:'start',}}>
+<ArrowLeftOutlined onClick={()=>{
+   navigate(-1)
+}} />
+
+       <div style={{display:'flex',flexDirection:'column',alignItems:'start',marginLeft:'19px'}}>
+        <p style={{fontWeight:'bold',fontSize:'18px'}}>Review Cart</p>
+        <p>Hiramani Store | Chunabhati</p>
+        </div>             
+</div>
     </>)
 }
 export default Cart;
